c70779c0d88d1d14dabb1a677482fb2c791bcb00
Dmitry Ivanov <dmitry.ivanov@blueacornici.com>
2023-03-07 11:10:52 -0500

    mac updates

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,5 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://www.garmin.com/en-US')
+    cy.visit('https://publish-p92655-e836164.adobeaemcloud.com/testing-content/qa-testing/regression-testing-dmitry')
   })
 })
